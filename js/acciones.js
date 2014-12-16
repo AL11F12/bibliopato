2c95cd20cfd6dc31a9a9dedeed1c4628fbc051da
AL02F12 <fede1ctuv@live.com>
2014-12-16 13:05:37 -0600

    uno

---
+++ js/acciones.js
@@ -0,0 +1,39 @@
+function buscarlibros ()
+{
+	$.ajax({
+		type:"POST",
+		url: "http://192.168.1.188/practica12/listado_general.php"
+	}).done(function(msg) {
+		alert (msg);
+		var DatosJSON=JSON.parse(msg);
+		
+		If (DatosJSON.datos==1)
+		{
+			$('#libros').empty();
+			for (var i=0; i <DatosJSON.libros.length; i++){
+			$('#libros').append('<div style="float:left;width:45%"> <p>Libro: '+DatosJSON.libros[i].Nombre_libro+' <br> Autor :'+DatosJSON.libros[i].Autor+' </p> </div> <div style="float:left;width:55%"> <img class="portada" src="http://192.168.1.188/practica12/fotos/'+DatosJSON.libros[i].Id+'.jpg">  </div> <div style="clear:both">  <hr> <hr> </div>');
+			}
+		}
+		
+		If (DatosJSON.datos==0)
+		{
+			alert("No hay Libros que mostrar");
+		}
+	});
+	
+	$('#page2').trigger('pagecreate');
+}
+
+
+
+
+
+$(document).ready(function(e) {
+ document.addEventListener("deviceready",function(){
+  $('#todos').tap(function(){
+    buscarlibros ();
+	$.mobile.changePage('#page2');
+  });
+ }); //deviceready
+}); //document ready
+
